c0a18a5561c07887bd65933a180c2721828edca8
AbhishekRauthan909 <abhishekrauthan37@gmail.com>
2021-02-16 23:00:20 +0530

    q5

--- Question5/update.php
+++ Question5/update.php
@@ -1,7 +1,6 @@
 <?php
 include('config.php');
 ?>
-
 <?php
 $id=$_GET['id'];
 $sql="SELECT * FROM `users` WHERE `id`=$id";
@@ -12,7 +11,6 @@ $email=$row['email'];
 $gender=$row['gender'];
 $city=$row['city'];
 ?>
-
 <?php
 if(isset($_POST['update']))
 {
@@ -32,32 +30,29 @@ if(isset($_POST['update']))
     }
 }
 ?>
-
-
-
 <!DOCTYPE html>
-<head>
-<title>
-Forms
-</title>
-</head>
-<body>
-<form method="POST" action="update.php?id=<?php echo "$id"?>">
-USERNAME <input type="text" name="username" value="<?php echo "$username"?>" placeholder="Enter your username" required><br><br>
-E-MAIL<input type="email" name="email" value="<?php echo "$email"?>" placeholder="Type your email" required><br><br>
-<input type="radio" name="gender" value="male" required <?php if($gender=="male") echo "checked"?>/>Male
-<input type="radio" name="gender" value="female" required <?php if($gender=="female") echo "checked"?>/>Female
-<select name="city" value=<?php echo "$city"?>>
-     <br>
-     Select your city
-        <option value="Rishikesh" <?php if($city=="Rishikesh"){echo "selected";}?>>Rishikesh</option>
-        <option value="Dehradun" <?php if($city=="Dehradun"){echo "selected";}?>>Dehradun</option>
-        <option value="Delhi" <?php if($city=="Delhi"){echo "selected";}?>>Delhi</option>
-        <option value="Mussoorie" <?php if($city=="Mussoorie"){echo "selected";}?>>Mussoorie</option>
-</select>
-<br>
-<br>
-<input type="submit" name="update" value="Click here to update your data">
-</form>
-</body>
+    <head>
+        <title>
+              Forms
+        </title>
+    </head>
+    <body>
+         <form method="POST" action="update.php?id=<?php echo "$id"?>">
+             USERNAME <input type="text" name="username" value="<?php echo "$username"?>" placeholder="Enter your username" required><br><br>
+             E-MAIL<input type="email" name="email" value="<?php echo "$email"?>" placeholder="Type your email" required><br><br>
+             <input type="radio" name="gender" value="male" required <?php if($gender=="male") echo "checked"?>/>Male
+            <input type="radio" name="gender" value="female" required <?php if($gender=="female") echo "checked"?>/>Female
+             <select name="city" value=<?php echo "$city"?>>
+             <br>
+             Select your city
+             <option value="Rishikesh" <?php if($city=="Rishikesh"){echo "selected";}?>>Rishikesh</option>
+             <option value="Dehradun" <?php if($city=="Dehradun"){echo "selected";}?>>Dehradun</option>
+             <option value="Delhi" <?php if($city=="Delhi"){echo "selected";}?>>Delhi</option>
+             <option value="Mussoorie" <?php if($city=="Mussoorie"){echo "selected";}?>>Mussoorie</option>
+            </select>
+        <br>
+         <br>
+            <input type="submit" name="update" value="Click here to update your data"> 
+            </form>
+    </body>
 </html>
